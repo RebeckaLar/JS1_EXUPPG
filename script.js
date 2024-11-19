543299c495acd8000958a0d40f103e88f0472611
Rebecka <rebecka.rl.larsson@gmail.com>
2024-11-19 15:55:11 +0100

    Fifth release, form validation added. Enough for passing grade?

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //Array som hämtar alla data från arrayen i API:et. Används för att kunna display
 const posts = [];
 
@@ -15,7 +16,7 @@ const getAllTodos = async() => {
         const data = await response.json()
 
         data.forEach(post => posts.push(post));
-        addToPosts(); //liknar renderPosts()
+        addToPosts();
 
         //Arrayen med data från API:et
         console.log(data);
@@ -29,8 +30,8 @@ const getAllTodos = async() => {
 }
 getAllTodos();
 
-//Adderar to-dos från API:ets array till egen array posts []
-function addToPosts() { //liknar renderPosts()
+//Adderar objekt (to-do's) från API:ets array till egen array Posts och visar på hemsidan[]
+function addToPosts() {
     let list = document.getElementById('list1');
     posts.forEach(obj => {
         let li = document.createElement('li');
@@ -38,6 +39,24 @@ function addToPosts() { //liknar renderPosts()
         li.innerText = obj.title;
     })
 }
+
+// //För eventuell PUT
+// function addToPosts() {
+//     let list = document.getElementById('list1');
+//     posts.forEach(obj => {
+//         let div = document.createElement('div');
+//         list.appendChild(div);
+//         let label = document.createElement('label')
+//         let item = document.createElement('input');
+//         label.setAttribute("for", "flexCheckDefault");
+//         item.setAttribute("id", "flexCheckDefault");
+//         item.setAttribute("type", "checkbox");
+//         item.setAttribute("class", "form-check-input");
+//         div.appendChild(label);
+//         div.appendChild(item);
+//         label.innerText = obj.title;
+//     })
+// }
 
 const createTodo = () => {
     const list = document.getElementById('list1');
@@ -48,30 +67,21 @@ const createTodo = () => {
     li.innerText = title;
     
     console.log('createTodo() done. Title value is ' + title);
-    const objTitle = title;
-    postTodo(objTitle);
-    
+    postTodo(title);
 }
 
-// const postTodo = () => {
-//     // createTodo();
-//     fetch('https://js1-todo-api.vercel.app/api/todos?apikey=1a450641-7efe-4a0f-bcb3-7e9f93cca42c', {
-//         method: 'POST',
-//         body : JSON.stringify(post),
-//         headers: {
-//             'Content-Type' : 'application/json' 
-//         }
-//     })
-//         .then(result => {
-//             if(Response.status = 201) {
-//                 // createTodo();
-//                 console.log('Status 201, to-do created');
-//             } 
-//             console.log(result);
-//             return result.json();
-//         })
-//         .then(data => console.log(data))
-//         .catch(err => console.log(err));
+//För eventuell PUT
+// const createTodo = () => {
+//     const list = document.getElementById('list1');
+//     const title = document.getElementById('title').value;
+
+//     let item = document.createElement('input');
+//     item.setAttribute("type", "checkbox");
+//     list.appendChild(item);
+//     item.innerText = title;
+    
+//     console.log('createTodo() done. Title value is ' + title);
+//     postTodo(title);
 // }
 
 //Triggas vid klick på knappen Create to-do
@@ -109,31 +119,102 @@ const postTodo = async (objTitle) => {
 const btnCreate = document.getElementById('btnCreate');
 btnCreate.addEventListener('click', (e) => {
     e.preventDefault();
-    const item = document.getElementById('title').value;
-    if (item == '' || item == null) {
-        console.log('Please write a title');
-    } else {
-        createTodo();
-    }
+    validateInput();
 })
 
 
 const btnDelete = document.getElementById('btnDelete');
 btnDelete.addEventListener('click', (e) => {
     e.preventDefault();
-    const item = document.getElementById('search').value;
-    console.log('Removing ' + item);
+    validateSearch();
+})
 
+const validateInput = () => {
+    console.log('validateInput()');
+    const title = document.getElementById('title');
+
+    if(title.value === '' || title.length < 0 || title == null) {
+        addErrorCreateTodo();
+    } else {
+        removeErrorCreateTodo();
+        createTodo();
+    }
+}
+
+// const validateSearch2 = () => {
+//     console.log('validateSearch()');
+//     const title = document.getElementById('title');
+
+//     if(title.value === '' || title.length < 0 || title == null) {
+//         addErrorDeleteTodo();
+//     } else {
+//         removeTodo();
+//     }
+// }
+
+const validateSearch = () => {
+    console.log('validateSearch()');
+    const search = document.getElementById('search').value;
+
+    let matched;
     posts.filter(obj => {
-        if (obj.title == item) {
-            console.log(obj.title);
+        if (obj.title == search) {
+            removeErrorDeleteTodo();
             removeTodo(obj);
-            return;
+            matched = true;
         } else {
-            console.log('Does not match any To-do title. Try again');
+            matched = false;
         }
     })
-})
+    
+    if(matched == false) {
+        addErrorDeleteTodo();
+    }
+}
+
+// const addErrorMessage = () => {
+//     const form = document.getElementById('form');
+//     const errMessage = 'Please write a title';
+//     const title = document.getElementById('title');
+//     let p = document.createElement('p');
+    
+//     form.appendChild(p);
+//     p.innerText = errMessage;
+//     title.classList.add('error');
+//     return;
+// }
+
+const addErrorCreateTodo = () => {
+    console.log('addErrorCreateTodo()');
+
+    const form = document.getElementById('createTodo');
+    form.classList.add('error');
+    return;
+}
+
+const addErrorDeleteTodo = () => {
+    console.log('addErrorDeleteTodo()');
+
+    const form = document.getElementById('deleteTodo');
+    form.classList.add('error');
+    return;
+}
+
+const removeErrorCreateTodo = () => {
+    console.log('removeErrorCreateTodo()');
+
+    const form = document.getElementById('createTodo');
+    form.classList.remove('error');
+    return;
+}
+
+const removeErrorDeleteTodo = () => {
+    console.log('removeErrorDeleteTodo()');
+
+    const form = document.getElementById('deleteTodo');
+    form.classList.remove('error');
+    return;
+}
 
 const removeTodo = async (id) => {
     const bla = id._id;
@@ -147,6 +228,8 @@ const removeTodo = async (id) => {
     console.log(response);
     const data = await response.json();
     console.log('Deleted todo with id:' + data);
-    window.location.reload(false);
+    window.location.reload();
     return data;
 }
+
+//Valideringvideon del 1 19:30
